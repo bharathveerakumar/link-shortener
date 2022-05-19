f9d4070e65a1c36d769d1c6c23f1186ecb9994fe
bharathveerakumar <bharathveerakumars@gmail.com>
2022-05-19 16:56:00 +0530

    A

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose')
 require('dotenv').config()
 
-mongoose.connect(`mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@bharath888.abfsc.mongodb.net/linkShort`, (err)=>{
+mongoose.connect(`mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@bharath888.abfsc.mongodb.net/?retryWrites=true&w=majority/linkShort`, (err)=>{
     !err?console.log("Db Success"):console.log('Db Failed');
 })
